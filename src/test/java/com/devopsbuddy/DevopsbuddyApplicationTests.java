6af264e2fa0b15711787593c1f603078c7ba2732
jlabeaga <jlabeaga@hotmail.com>
2016-12-03 20:34:12 +0100

    Added logging setup with logback

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -2,6 +2,8 @@ package com.devopsbuddy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,6 +21,7 @@ public class DevopsbuddyApplicationTests {
 	
 	@Test
 	public void testMessageByLocaleService() throws Exception {
+		
 		String expectedResult = "Bootstrap starter template";
 		String messageId = "index.main.callout";
 		String actual = i18NService.getMessage(messageId);
