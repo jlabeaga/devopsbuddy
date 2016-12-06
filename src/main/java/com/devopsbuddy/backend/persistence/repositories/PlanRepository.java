a4072efdc1b85497434bb547c79a996b51d08a15
jlabeaga <jlabeaga@hotmail.com>
2016-12-06 20:44:28 +0100

    Set up the H2 console and created the repository layer

---
+++ src/main/java/com/devopsbuddy/backend/persistence/repositories/PlanRepository.java
@@ -0,0 +1,12 @@
+package com.devopsbuddy.backend.persistence.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devopsbuddy.backend.persistence.domain.backend.Plan;
+import com.devopsbuddy.backend.persistence.domain.backend.User;
+
+@Repository
+public interface PlanRepository extends CrudRepository<Plan, Integer> {
+
+}
---
+++ src/test/java/com/devopsbuddy/test/integration/RepositoriesIntegrationTest.java
@@ -0,0 +1,152 @@
+package com.devopsbuddy.test.integration;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.devopsbuddy.DevopsbuddyApplication;
+import com.devopsbuddy.backend.persistence.domain.backend.Plan;
+import com.devopsbuddy.backend.persistence.domain.backend.Role;
+import com.devopsbuddy.backend.persistence.domain.backend.User;
+import com.devopsbuddy.backend.persistence.domain.backend.UserRole;
+import com.devopsbuddy.backend.persistence.repositories.PlanRepository;
+import com.devopsbuddy.backend.persistence.repositories.RoleRepository;
+import com.devopsbuddy.backend.persistence.repositories.UserRepository;
+import com.devopsbuddy.enums.PlansEnum;
+import com.devopsbuddy.enums.RolesEnum;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DevopsbuddyApplication.class)
+public class RepositoriesIntegrationTest {
+
+    @Autowired
+    protected PlanRepository planRepository;
+
+    @Autowired
+    protected RoleRepository roleRepository;
+
+    @Autowired
+    protected UserRepository userRepository;
+
+    
+    @Before
+    public void init() {
+        Assert.assertNotNull(planRepository);
+        Assert.assertNotNull(roleRepository);
+        Assert.assertNotNull(userRepository);
+    }
+
+    @Test
+    public void testCreateNewPlan() throws Exception {
+        Plan basicPlan = createPlan(PlansEnum.BASIC);
+        planRepository.save(basicPlan);
+        Plan retrievedPlan = planRepository.findOne(PlansEnum.BASIC.getId());
+        Assert.assertNotNull(retrievedPlan);
+    }
+
+    @Test
+    public void testCreateNewRole() throws Exception {
+
+        Role userRole  = createRole(RolesEnum.BASIC);
+        roleRepository.save(userRole);
+
+        Role retrievedRole = roleRepository.findOne(RolesEnum.BASIC.getId());
+        Assert.assertNotNull(retrievedRole);
+    }
+
+    @Test
+    public void testCreateNewUser() throws Exception {
+
+        Plan basicPlan = createPlan(PlansEnum.BASIC);
+        planRepository.save(basicPlan);
+
+        User basicUser = createBasicUser();
+        basicUser.setPlan(basicPlan);
+
+        Role basicRole = createRole(RolesEnum.BASIC);
+        roleRepository.save(basicRole);
+
+        Set<UserRole> userRoles = new HashSet<>();
+        UserRole userRole = new UserRole(basicUser, basicRole);
+        userRoles.add(userRole);
+
+        // ALWAYS save a collection of related entitites in this way:
+        // first, get the collection, then add the entity
+        basicUser.getUserRoles().addAll(userRoles);
+        
+        basicUser = userRepository.save(basicUser);
+
+        User newlyCreatedUser = userRepository.findOne(basicUser.getId());
+        Assert.assertNotNull(newlyCreatedUser);
+        Assert.assertTrue(newlyCreatedUser.getId() != 0);
+        Assert.assertNotNull(newlyCreatedUser.getPlan());
+        Assert.assertNotNull(newlyCreatedUser.getPlan().getId());
+        Set<UserRole> newlyCreatedUserUserRoles = newlyCreatedUser.getUserRoles();
+        for (UserRole ur : newlyCreatedUserUserRoles) {
+            Assert.assertNotNull(ur.getRole());
+            Assert.assertNotNull(ur.getRole().getId());
+        }
+
+    }
+    
+    protected Plan createPlan(PlansEnum plansEnum) {
+        return new Plan(plansEnum);
+    }
+
+    protected Role createRole(RolesEnum rolesEnum) {
+        return new Role(rolesEnum);
+    }
+
+    protected User createUser(String username, String email) {
+        Plan basicPlan = createPlan(PlansEnum.BASIC);
+        planRepository.save(basicPlan);
+
+        User basicUser = createBasicUser(username, email);
+        basicUser.setPlan(basicPlan);
+
+        Role basicRole = createRole(RolesEnum.BASIC);
+        roleRepository.save(basicRole);
+
+        Set<UserRole> userRoles = new HashSet<>();
+        UserRole userRole = new UserRole(basicUser, basicRole);
+        userRoles.add(userRole);
+
+        basicUser.getUserRoles().addAll(userRoles);
+        basicUser = userRepository.save(basicUser);
+        return basicUser;
+    }
+    
+    public User createBasicUser() {
+
+        User user = new User();
+        user.setUsername("BasicUserName");
+        user.setPassword("secret");
+        user.setEmail("BasicUserEmail@company.com");
+        user.setFirstName("firstName");
+        user.setLastName("lastName");
+        user.setPhoneNumber("123456789123");
+        user.setCountry("GB");
+        user.setEnabled(true);
+        user.setDescription("A basic user");
+        user.setProfileImageUrl("https://blabla.images.com/basicuser");
+
+        return user;
+    }
+
+    public User createBasicUser(String username, String email) {
+
+        User user = createBasicUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        return user;
+    }
+    
+    
+}
