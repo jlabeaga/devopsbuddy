a4a3c3e404e29a77f30d1eb4ef9a67a1217068e8
jlabeaga <jlabeaga@hotmail.com>
2016-12-06 12:27:05 +0100

    Createcd the JPA entity model for Spring Security

---
+++ src/main/java/com/devopsbuddy/backend/persistence/domain/backend/Plan.java
@@ -0,0 +1,42 @@
+package com.devopsbuddy.backend.persistence.domain.backend;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Plan implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+	
+	private String name;
+	
+	public Plan() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Plan [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+}
