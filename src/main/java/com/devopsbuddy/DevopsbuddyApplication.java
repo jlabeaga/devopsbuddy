6d7e5bdfe495654fd0074ca03d0a5fc59bd0df7f
jlabeaga <jlabeaga@hotmail.com>
2016-12-08 21:25:29 +0100

    Added post method to signup controller

--- src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -1,8 +1,13 @@
 package com.devopsbuddy;
 
-import java.util.HashSet;
-import java.util.Set;
-
+import com.devopsbuddy.backend.persistence.domain.backend.Role;
+import com.devopsbuddy.backend.persistence.domain.backend.User;
+import com.devopsbuddy.backend.persistence.domain.backend.UserRole;
+import com.devopsbuddy.backend.service.PlanService;
+import com.devopsbuddy.backend.service.UserService;
+import com.devopsbuddy.enums.PlansEnum;
+import com.devopsbuddy.enums.RolesEnum;
+import com.devopsbuddy.utils.UserUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +15,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.devopsbuddy.backend.persistence.domain.backend.Role;
-import com.devopsbuddy.backend.persistence.domain.backend.User;
-import com.devopsbuddy.backend.persistence.domain.backend.UserRole;
-import com.devopsbuddy.backend.service.UserService;
-import com.devopsbuddy.enums.PlansEnum;
-import com.devopsbuddy.enums.RolesEnum;
-import com.devopsbuddy.utils.UserUtils;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class DevopsbuddyApplication implements CommandLineRunner {
@@ -29,9 +28,12 @@ public class DevopsbuddyApplication implements CommandLineRunner {
 	@Autowired
 	private UserService userService;
 
+	@Autowired
+	private PlanService planService;
+
 	@Value("${webmaster.username}")
 	private String webmasterUsername;
-	
+
 	@Value("${webmaster.password}")
 	private String webmasterPassword;
 
@@ -41,9 +43,13 @@ public class DevopsbuddyApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsbuddyApplication.class, args);
 	}
-	
+
 	@Override
 	public void run(String... args) throws Exception {
+
+		LOG.info("Creating Basic and Pro plans in the database...");
+		planService.createPlan(PlansEnum.BASIC.getId());
+		planService.createPlan(PlansEnum.PRO.getId());
 
 		User user = UserUtils.createBasicUser(webmasterUsername, webmasterEmail);
 		user.setPassword(webmasterPassword);
@@ -53,5 +59,4 @@ public class DevopsbuddyApplication implements CommandLineRunner {
 		userService.createUser(user, PlansEnum.PRO, userRoles);
 		LOG.info("User {} created", user.getUsername());
 	}
-	
 }
