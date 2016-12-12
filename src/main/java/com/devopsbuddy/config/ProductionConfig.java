4f1187b149279d933a34178f4b4dd6a9bef895e3
jlabeaga <jlabeaga@hotmail.com>
2016-12-12 23:05:18 +0100

    Added support for MySQL

--- src/main/java/com/devopsbuddy/config/ProductionConfig.java
+++ src/main/java/com/devopsbuddy/config/ProductionConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 import com.devopsbuddy.backend.service.EmailService;
-import com.devopsbuddy.backend.service.MockEmailService;
 import com.devopsbuddy.backend.service.SmtpEmailService;
 
 @Configuration
